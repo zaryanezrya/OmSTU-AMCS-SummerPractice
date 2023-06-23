d7f426664dd32e43a1c525dcc05c387260ed764d
The flower collector under the bloody rain <61908871+ValikovaLiza@users.noreply.github.com>
2023-06-23 15:26:08 +0600

    Update SquareEquation.cs

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -4,6 +4,17 @@ public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
+        int kvadr(double b, double c, double *x1, double *x2)
+        {
+            double d;
+            d=b*b-4*c;
+            if (d<0) return 0;
+            *x1=(-b+sqrt(d))/2;
+            *x2=(-b-sqrt(d))/2;
+            x1=-(b+sign(b)*sqrt(d))/2;
+            x2=c/x1
+            return 1;
+        }
         throw new NotImplementedException();
     }
 }
