082556075ba8d58f8dfeebb3ff71a33a2787e4cd
Ivan Sharun <ivan@sha.run>
2023-06-23 10:33:08 +0600

    Add SquareEquationLib project

---
+++ SquareEquationLib/SquareEquation.cs
@@ -0,0 +1,9 @@
+﻿namespace SquareEquationLib;
+
+public class SquareEquation
+{
+    public static double[] Solve(double a, double b, double c)
+    {
+        throw new NotImplementedException();
+    }
+}
