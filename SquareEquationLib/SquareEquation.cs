5e4cdda43d3d14fa4825b39feef04c6c35901381
Chixablox <smrdak152@gmail.com>
2023-06-23 11:39:56 +0000

    C1

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -1,9 +1,32 @@
-﻿namespace SquareEquationLib;
+﻿using System;
+
+namespace SquareEquationLib;
 
 public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
-        throw new NotImplementedException();
+        double positive = double.PositiveInfinity;
+        double negative = double.NegativeInfinity;
+        double NaN = 0.0/0.0;
+        if ((a==0) || (a==positive) || (a==negative) || (a==NaN))
+        {
+            throw new System.ArgumentException();
+        }
+        else if ((b==positive) || (b==negative) || (b==NaN))
+        {
+            throw new System.ArgumentException();
+        }
+        else if ((c==positive) || (c==negative) || (c==NaN))
+        {
+            throw new System.ArgumentException();
+        }
+        else
+        {
+            double x1 = -(b + Math.Sign(b)*Math.Sqrt(Math.Pow(b,2)-4*a*c))/2;
+            double x2 = c/x1;
+            double[] ans = new double[2]{x1,x2};
+            return ans;
+        }
     }
 }
