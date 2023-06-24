eaefeb7805dd143d72feab498e8bb38281b66b4d
Elizaveta <lili5valikova@gmail.com>
2023-06-24 06:42:10 +0000

    SquareEquation

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -4,17 +4,33 @@ public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
-        int kvadr(double b, double c, double *x1, double *x2)
-        {
-            double d;
-            d=b*b-4*c;
-            if (d<0) return 0;
-            *x1=(-b+sqrt(d))/2;
-            *x2=(-b-sqrt(d))/2;
-            x1=-(b+sign(b)*sqrt(d))/2;
-            x2=c/x1
-            return 1;
+        double[] Result = new double[2];
+        double D,x1,x2;
+        D=(b*b)-(4*a*c);
+        double epsilon=1e-9;
+
+        if ((-epsilon<a)&&(a<epsilon)) 
+            throw new System.ArgumentException(); 
+        if ((Double.IsNaN(a)) || (Double.IsNegativeInfinity(a)) || (Double.IsPositiveInfinity(a))||(Math.Abs(a)==0) ) 
+            throw new ArgumentException();
+        if ((Double.IsNaN(b)) || (Double.IsNegativeInfinity(b)) || Double.IsPositiveInfinity(b)) 
+            throw new ArgumentException();
+        if ((Double.IsNaN(c)) || (Double.IsNegativeInfinity(c)) || Double.IsPositiveInfinity(c)) 
+            throw new ArgumentException();
+           
+        
+        if (D<=-epsilon){
+           Result = new double[0];
         }
-        throw new NotImplementedException();
+        else if ((-epsilon<D) && (D<epsilon)){
+            x1=-b/(2*a);
+            Result = new double[] {x1};
+        }
+        else{
+            x1 = (2*c)/-(b+Math.Sign(b)*Math.Sqrt(D));
+            x2 = c / (a*x1);
+            Result = new double[] { x1, x2 };
+        }
+        return Result;
     }
 }
