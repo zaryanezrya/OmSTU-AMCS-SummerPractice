d4b348b7f58cfb7b910126689e2b28d6b2ba2fe2
Egor-Kozh <kozhevnikovegor55@gmail.com>
2023-06-24 18:17:13 +0000

    123

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -4,6 +4,48 @@ public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
-        throw new NotImplementedException();
+        double[] answer = new double[0];
+        double x1;
+        double x2;
+
+        if (Double.IsPositiveInfinity(a) || Double.IsNegativeInfinity(a) || Math.Abs(a) < Double.Epsilon || Double.IsNaN(a))
+        {
+            throw new System.ArgumentException();
+        }
+        if (Double.IsPositiveInfinity(b) || Double.IsNegativeInfinity(b) || Double.IsNaN(b))
+        {
+            throw new System.ArgumentException();
+        }
+        if (Double.IsPositiveInfinity(c) || Double.IsNegativeInfinity(c) || Double.IsNaN(c))
+        {
+            throw new System.ArgumentException();
+        }
+
+        double D = (b * b) - (4.0 * a * c);
+        
+        if (D > -Double.Epsilon && D < Double.Epsilon)
+        {
+            answer = new double[1];
+
+            D = 0;
+
+            x1 = -(b + Math.Sign(b) * Math.Sqrt(D)) / 2 ;
+
+            answer[0] = x1;                  
+        }
+        if(D >= Double.Epsilon)
+        {
+            answer = new double[2];
+            
+            x1 = -(b + Math.Sign(b) * Math.Sqrt(D)) / 2 ;
+            x2 = c / x1;
+
+            answer[0] = x1;
+            answer[1] = x2;
+        }
+
+        return answer;
     }
 }
+
+
