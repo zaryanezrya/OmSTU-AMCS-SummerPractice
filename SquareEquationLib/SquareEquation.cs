ccdf72657b6066a8f061d9f20a6fecffc99390a4
IvanyutinSA <ivanyutin2004@gmail.com>
2023-06-24 06:35:57 +0000

    update

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -3,7 +3,28 @@
 public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
-    {
-        throw new NotImplementedException();
+    {        
+        const double TOLERANCE = 0.000000001;
+
+        if (a < TOLERANCE)
+            throw new System.ArgumentException();
+
+        foreach (double x in new double[] {a, b, c})
+        {
+            if (double.IsNaN(x) || double.IsInfinity(x))
+                throw new System.ArgumentException();
+        }
+
+        double D = b*b - 4*a*c;        
+        if (Math.Sign(D) < 0 && !(Math.Abs(D) < TOLERANCE))
+            return new double[0];
+
+        double x1 = -(b + Math.Sign(b)*Math.Sqrt(D))/2;
+        double x2 = c/x1;
+
+        if (D < TOLERANCE)
+            return new double[] {x1};
+        
+        return new double[] {x1, x2};   
     }
 }
