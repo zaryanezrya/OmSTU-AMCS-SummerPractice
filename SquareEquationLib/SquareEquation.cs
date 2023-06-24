a1ac8b2fe10d0ca6a301df920e59c92187ee1a0e
Chixablox <smrdak152@gmail.com>
2023-06-24 04:58:47 +0000

    6/8

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -9,38 +9,36 @@ public class SquareEquation
         double positive = double.PositiveInfinity;
         double negative = double.NegativeInfinity;
         double eps = double.Epsilon;
-        double NaN = 0.0/0.0;
-        if ((a==0) ||(a==positive) || (a==negative) || (a==NaN))
+        double NaN = double.NaN;
+        if (a==0)
         {
             throw new System.ArgumentException();
+        }
+        else if ((a==positive) || (a==negative) || (a==NaN))
+        {
+            throw new ArgumentException();
         }
         else if ((b==positive) || (b==negative) || (b==NaN))
         {
-            throw new System.ArgumentException();
+            throw new ArgumentException();
         }
-        if ((c==positive) || (c==negative) || (c==NaN))
+        else if ((c==positive) || (c==negative) || (c==NaN))
         {
-            throw new System.ArgumentException();
+            throw new ArgumentException();
         }
         else
         {
-            double D = Math.Sqrt(Math.Pow(b,2)-4*a*c);
-            if(D>0)
+            double D = Math.Pow(b,2)-4*a*c;
+            if((D>0) /*|| ((Math.Abs(D)<eps) && (D!=0))*/)
             {
-                double x1 = -(b + Math.Sign(b)*D)/2;
+                double x1 = -(b + Math.Sign(b)*Math.Sqrt(D))/2;
                 double x2 = c/x1;
                 double[] ans = new double[2]{x1,x2};
                 return ans;
             }
-            else if (D==0)
+            else if(D==0)
             {
-                double x1 = -(b + Math.Sign(b)*D)/2;
-                double[] ans = new double[1]{x1};
-                return ans;
-            }
-            else if((-1*eps<D) && (D<0))
-            {
-                double x1 = -(b + Math.Sign(b)*D)/2;
+                double x1 = -(b + Math.Sign(b)*Math.Sqrt(D))/2;
                 double[] ans = new double[1]{x1};
                 return ans;
             }
