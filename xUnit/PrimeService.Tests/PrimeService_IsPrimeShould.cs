73da1c6bd6f9fb7cbab086cbda2ecf57c20dbd1a
Chixablox <smrdak152@gmail.com>
2023-06-25 11:31:25 +0000

    Test?

--- xUnit/PrimeService.Tests/PrimeService_IsPrimeShould.cs
+++ xUnit/PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -1,25 +1,49 @@
 using Xunit;
-using Prime.Services;
+using SquareEquationLib;
+
 
 namespace Prime.UnitTests.Services
 {
     public class PrimeService_IsPrimeShould
     {
-        private readonly PrimeService _primeService;
-
-        public PrimeService_IsPrimeShould()
+        [Fact]
+        public void Test1()
         {
-            _primeService = new PrimeService();
+            var squareEquation = new SquareEquation();
+            double[] ans = squareEquation.Solve(1,3,-4);
+            double[] r_ans = new double[2]{-4,1};
+
+            Assert.Equal(r_ans, ans);
         }
-        [Theory]
-        [InlineData(-1)]
-        [InlineData(0)]
-        [InlineData(1)]
-        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
-        {
-            var result = _primeService.IsPrime(value);
 
-            Assert.False(result, $"{value} should not be prime");
+        [Fact]
+        public void Test2()
+        {
+            var squareEquation = new SquareEquation();
+            double[] ans = squareEquation.Solve(1,-4,4);
+            double[] r_ans = new double[1]{2};
+
+            Assert.Equal(r_ans, ans);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var squareEquation = new SquareEquation();
+            double[] ans = squareEquation.Solve(1,-5,9);
+            double[] r_ans = new double[]{};
+
+            Assert.Equal(r_ans, ans);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            var squareEquation = new SquareEquation();
+            double[] ans = squareEquation.Solve(0,-5,9);
+            double[] r_ans = new double[]{};
+
+            Assert.Equal(r_ans, ans);
         }
     }
 }
