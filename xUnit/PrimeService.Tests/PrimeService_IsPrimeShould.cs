4191920c143afb445716b2a63f4fb2a55fcdd6d8
Chixablox <smrdak152@gmail.com>
2023-06-25 11:47:04 +0000

    All test&

--- xUnit/PrimeService.Tests/PrimeService_IsPrimeShould.cs
+++ xUnit/PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -40,10 +40,27 @@ namespace Prime.UnitTests.Services
         public void Test4()
         {
             var squareEquation = new SquareEquation();
-            double[] ans = squareEquation.Solve(0,-5,9);
-            double[] r_ans = new double[]{};
+            Assert.Throws<ArgumentException>(() => squareEquation.Solve(0,-5,9));
+        }
 
-            Assert.Equal(r_ans, ans);
+        [Fact]
+        public void Test5()
+        {
+            var squareEquation = new SquareEquation();
+            Assert.Throws<ArgumentException>(() => squareEquation.Solve(double.PositiveInfinity,-5,9));
+        }
+        [Fact]
+        public void Test6()
+        {
+            var squareEquation = new SquareEquation();
+            Assert.Throws<ArgumentException>(() => squareEquation.Solve(1,double.NaN,4));
+        }
+
+        [Fact]
+        public void Test7()
+        {
+            var squareEquation = new SquareEquation();
+            Assert.Throws<ArgumentException>(() => squareEquation.Solve(1,-4,double.NegativeInfinity));
         }
     }
 }
