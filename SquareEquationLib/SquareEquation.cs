c2e8d23bf0c5cb50433db9ef9da9e27bb6c0906b
Daria <ladydashaberezina@mail.ru>
2023-06-26 13:52:39 +0000

    Fix

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -17,26 +17,26 @@ public class SquareEquation
         {
             throw new ArgumentException();
         }
-        if (Math.Abs(d) < 1e-10)
-        {
-            double x1 = (-b + Math.Sqrt(d)) / 2;
-            roots = new double[1];
-            roots[0] = x1;
-            return roots;
-        }
-        else if (d < 0)
-        {
-            return roots;
-        }
-        else
+        if (d > 0)
         {
             double x1 = (-b + Math.Sqrt(d)) / 2;
             double x2 = (-b - Math.Sqrt(d)) / 2;
             roots = new double[2];
             roots[0] = x1; roots[1] = x2;
             return roots;
-
         }
+        else if (d < -double.Epsilon)
+        {
+            double x1 = (-b + Math.Sqrt(d)) / 2;
+            roots = new double[1];
+            roots[0] = x1;
+            return roots;
+        }
+        else
+        {
+            return roots;
+        }
+
        
     }
 
