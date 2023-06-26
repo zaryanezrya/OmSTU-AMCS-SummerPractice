0a56e170002c0e536d4c17b26ec4ef45b9ba4977
Daria <ladydashaberezina@mail.ru>
2023-06-26 17:37:59 +0000

    Fix

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -7,7 +7,7 @@ public class SquareEquation
     public static double[] Solve(double a, double b, double c)
     {
         double d;
-        double[] roots = {};
+        double[] roots = Array.Empty<double>();
         d = b * b - 4 * a * c;
 
         if (Math.Abs(a) < double.Epsilon)
@@ -23,7 +23,7 @@ public class SquareEquation
         if (d >= double.Epsilon)
         {
             double x1 = -(b + Math.Sign(b) * Math.Sqrt(d)) / (2 * a);
-            double x2 = c/x1;
+            double x2 = c / x1;
             roots = new double[2];
             roots[0] = x1; roots[1] = x2;
             return roots;
