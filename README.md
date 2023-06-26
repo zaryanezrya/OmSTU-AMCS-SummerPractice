97b7ee456821015f7c54465b8dcaadb561d12cf5
SSEELLLLEERRSSDD <116637072+SSEELLLLEERRSSDD@users.noreply.github.com>
2023-06-26 23:28:05 +0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# OmSTU-AMCS-SummerPractice2023
+[![square equation test](https://github.com/Tintanaa/OmSTU-AMCS-SummerPractice2023/actions/workflows/checkontests.yml/badge.svg)](https://github.com/Tintanaa/OmSTU-AMCS-SummerPractice2023/actions/workflows/checkontests.yml)
--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -4,36 +4,34 @@ public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
+        double x1;
+        double x2;
+        double[] array = new double[2];
         double eps = 1e-9;
-        double[] no_roots = new double[] { };
-        
-        
-        if (-eps < a && a < eps || new[] { a, b, c }.Any(double.IsNaN) || new[] { a, b, c }.Any(double.IsInfinity))
+        double d = b * b - 4 * a * c;
+        if (-eps < a && a < eps)
         {
-            throw new ArgumentException();
+            throw new System.ArgumentException();
         }
-
-        double D = b * b - 4 * a * c;
-        
-        if (D <= -eps) 
+        if (a == 0 || new[] { a, b, c }.Any(double.IsNaN) || new[] { a, b, c }.Any(double.IsInfinity))
         {
-            return no_roots;
+            throw new ArgumentException("Ошибка");
         }
-        else
+        if (d<=-eps)
         {
-            if (-eps < D && D < eps)
-            { 
-                double[] root = new double[1];
-                root[0] = (-b) / 2 * a;
-                return root;
-            }
-            else
-            {
-                double[] roots = new double[2];
-                roots[0] = -(b + Math.Sign(b) * Math.Sqrt(D)) / 2;
-                roots[1] = c / roots[0];
-                return roots;
-            }
+            array = new double[0];
         }
+        else if (-eps < d && d < eps)
+        {
+            x1 = -b/(2*a);
+            array = new double[] { x1 };
+        }
+        else 
+        {
+            x1 = (2*c)/-(b+Math.Sign(b)*Math.Sqrt(d));
+            x2 = c / (a*x1);
+            array = new double[] { x1, x2 };
+        }
+        return array;
     }
 }
