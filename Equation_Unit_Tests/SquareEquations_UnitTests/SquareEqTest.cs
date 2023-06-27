4c9821231b959126cb8593ec3bcd7184aacc245e
Den <mr.dgmironov@mail.ru>
2023-06-27 23:50:35 +0300

    d

--- Equation_Unit_Tests/SquareEquations_UnitTests/SquareEqTest.cs
+++ Equation_Unit_Tests/SquareEquations_UnitTests/SquareEqTest.cs
@@ -1,53 +1,60 @@
 using Xunit;
 using SquareEquationLib;
+namespace SquareEquations_UnitTests;
 
-namespace SquareEquation_Test
+public class UnitTest1
 {
-    public class SquareEquation_Test
+    [Fact]
+    public void FistArgumentZero()
     {
-
+        Assert.Throws<ArgumentException>(() => SquareEquation.Solve(0, 0, 0));
+    }
     [Theory]
     [InlineData(double.NaN,1,1)]
     [InlineData(1,double.NaN,1)]
     [InlineData(1,1,double.NaN)]
-
     [InlineData(double.PositiveInfinity,1,1)]
     [InlineData(1,double.PositiveInfinity,1)]
     [InlineData(1,1,double.PositiveInfinity)]
-
     [InlineData(double.NegativeInfinity,1,1)]
     [InlineData(1,double.NegativeInfinity,1)]
     [InlineData(1,1,double.NegativeInfinity)]
-    public void testValidArgument(double a,double b, double c)
+    public void AnyInvalidArgument(double a,double b, double c)
     {
         Assert.Throws<ArgumentException>(() => SquareEquation.Solve(a, b, c));
     }
+    [Fact]
+    public void D_CloseToZero()
+    {
+        double[] expected_roots = new double[]{-0.5};
+        double[] roots = SquareEquation.Solve(1,1,0.24999999995);
+        if (roots.Length!=expected_roots.Length)
+        {
+            Assert.Fail("Amount of roots does not match");
+        }
+        Assert.Equal(roots[0],expected_roots[0]);
 
+    }
     [Theory]
-        [InlineData(1, 6, 9, new double[]{-3})]
-        [InlineData(10, 2, 10, new double[] {})]
-        [InlineData(1,2,-3, new double[] {1, -3})]
-        [InlineData(1, 0, 1, new double[]{})]
-        [InlineData(1, 4, 5, new double[]{})]
+        [InlineData(1, 0, 1, new double[] {})]
+        [InlineData(1,4,4, new double[] {-2})]
+        [InlineData(1,-3,2, new double[] {1, 2})]
 
-        public void ValidFindRoot(double a, double b, double c, double[] expected)
+        public void RootCorrectness(double a, double b, double c, double[] expectedRoots)
         {
             
-        double[] actual = SquareEquation.Solve(a, b, c);
+            double[] roots = SquareEquation.Solve(a, b, c);
+            Array.Sort(roots);
+            Array.Sort(expectedRoots);
 
-
-            Array.Sort(actual);
-            Array.Sort(expected);
-
-            if (expected.Length != actual.Length)
+            if (expectedRoots.Length != roots.Length)
             {
                 Assert.Fail("Amount of roots does not match");
             }
 
-            for (int i = 0; i < expected.Length; i++)
+            for (int i = 0; i < expectedRoots.Length; i++)
             {
-                Assert.Equal(expected[i], actual[i], 6);
+                Assert.Equal(expectedRoots[i], roots[i], 6);
             }
         }
-    }
 }
