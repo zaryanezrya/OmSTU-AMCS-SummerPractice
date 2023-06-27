85af119befec74880ecdc98ec87c2189d44c3807
Arina <15days.before@gmail.com>
2023-06-27 23:11:17 +0600

    xUnix

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -3,42 +3,30 @@ public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
-        double[] Result = new double[2];
-        double D,x1,x2;
-        D=(b*b)-(4*a*c);
         double epsilon=1e-9;
+        double D=b * b - 4 *a*c;
+        double[] Result1 = new double[] { };
 
-        if ((-epsilon<a)&&(a<epsilon))
-        {
-            throw new System.ArgumentException("Ошибочка вышла"); 
-        }             
-        if ((Double.IsNaN(a)) || (Double.IsNegativeInfinity(a)) || (Double.IsPositiveInfinity(a))||(Math.Abs(a)==0) ) 
+        if (-epsilon < a && a < epsilon) 
         {
             throw new ArgumentException("Ошибочка вышла");
         }
-        if ((Double.IsNaN(b)) || (Double.IsNegativeInfinity(b)) || Double.IsPositiveInfinity(b)) 
-        {
+        if ((-epsilon < a) && (a < epsilon) || (double.IsInfinity(a)) || (double.IsInfinity(b)) || (double.IsInfinity(c)) || (double.IsNaN(a)) || (double.IsNaN(b)) || (double.IsNaN(c))) 
             throw new ArgumentException("Ошибочка вышла");
-        }
-        if ((Double.IsNaN(c)) || (Double.IsNegativeInfinity(c)) || Double.IsPositiveInfinity(c)) 
-        {
-             throw new ArgumentException("Ошибочка вышла");
-        }
-        if (D<=-epsilon)
-        {
-           Result = new double[0];
-        }
-        else if ((-epsilon<D) && (D<epsilon))
-        {
-            x1=-b/(2*a);
-            Result = new double[] {x1};
+        
+        if (D <= -epsilon) return Result1;
+        else if (-epsilon < D & D < epsilon)
+        { 
+            double[] Result2 = new double[1];
+            Result2[0] = -(b) / 2*a;
+            return Result2;
         }
         else
         {
-            x1 = (2*c)/-(b+Math.Sign(b)*Math.Sqrt(D));
-            x2 = c / (a*x1);
-            Result = new double[] { x1, x2 };
+            double[] Result3 = new double[2];
+            Result3[0] = -(b + Math.Sign(b) * Math.Sqrt(D)) / 2;
+            Result3[1] = c / Result3[0];
+            return Result3;
         }
-        return Result;
     }
 }
---
+++ XUnit.Coverlet.Collector/UnitTest1.cs
@@ -0,0 +1,50 @@
+using Xunit;
+using SquareEquationLib;
+
+namespace SquareEquationLib.Tests;
+
+public class SquareEquationLib_isUnite
+{
+    [Fact]
+   public void TwoRoots()
+    {
+        double[] expected = new double[] { 9, 4 };
+        double[] actual = SquareEquation.Solve(1, -13, 36);
+
+        Assert.Equal(expected, actual);
+    }
+    [Fact]
+    public void OneRoot()
+    {
+        double[] expected = new double[] { -2 };
+        double[] actual = SquareEquation.Solve(1, 4, 4);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Nothing()
+    {
+        double[] expected = new double[] { };
+        double[] actual = SquareEquation.Solve(4, 2, 8);
+
+        Assert.Equal(expected, actual);
+    }
+
+
+
+    [Fact]
+    public void ThrowsArgumentException()
+    {
+        Assert.Throws<System.ArgumentException>(() => SquareEquation.Solve(0, 2, 3));
+    }
+
+    [Theory]
+    [InlineData(double.NaN, 2, 3)]
+    [InlineData(1, double.PositiveInfinity, 3)]
+    [InlineData(1, 2, double.NegativeInfinity)]
+    public void WrongCoefficients(double a, double b, double c)
+    {
+        Assert.Throws<System.ArgumentException>(() => SquareEquation.Solve(a, b, c));
+    }
+}
