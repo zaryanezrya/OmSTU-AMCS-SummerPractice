003dfb5ce4edc03caec72e0ee88fe4db839b824e
Zadorozhnyi <sz1253265@gmail.com>
2023-06-27 11:06:37 +0000

    first try

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -4,6 +4,44 @@ public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
-        throw new NotImplementedException();
+        double eps = 0.000000001;
+        double[] answer;
+
+        if (Math.Abs(a) < eps)
+        {
+            throw new System.ArgumentException();
+        }
+
+        foreach (double x in new double[] { a, b, c })
+        {
+            if (double.IsNaN(x) || double.IsInfinity(x))
+            {
+                throw new System.ArgumentException();
+            }
+        }
+
+        double d = Math.Pow(b, 2) - 4 * a * c;
+
+        if (d < 0 && !(Math.Abs(d) < eps))
+        {
+            answer = new double[0];
+
+        }
+        else if (d < eps)
+        {
+            answer = new double[1];
+            answer[0] = -(b + Math.Sign(b) * Math.Sqrt(d)) / 2;
+
+        }
+        else
+        {
+            answer = new double[2];
+            answer[0] = -(b + Math.Sign(b) * Math.Sqrt(d)) / 2;
+            answer[1] = c / answer[0];
+
+        }
+
+        return answer;
+
     }
 }
