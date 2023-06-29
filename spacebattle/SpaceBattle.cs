d25fd324caf1248f34ed3889fce1dfb3af13415c
Chixablox <smrdak152@gmail.com>
2023-06-29 11:26:48 +0000

    Мда

--- spacebattle/SpaceBattle.cs
+++ spacebattle/SpaceBattle.cs
@@ -1,35 +1,17 @@
 ﻿namespace SpaceBattle;
 public class SpaceBattle
 {
-    private double[] position = new double[2]{double.NaN, double.NaN};
-    private double[] speed = new double[2]{double.NaN, double.NaN};
-    private bool possibility_of_movement = true;
-
-    public SpaceBattle(double x, double y, double x_speed, double y_speed, bool possibility_of_movement)
-    {
-        double n_position = new double[2]{x,y};
-        double n_speed = new double[2]{x_speed,y_speed};
-        this.position = n_position;
-        this.speed = n_speed;
-        this.possibility_of_movement = possibility_of_movement;
-    }
-
-    public double[] Movement (double[] position, double[] speed, bool possibility_of_movement)
-    {
-        if(position[0]==NaN & position[1]=double.Nan)
-        {
+    public double[] Movement (double[] position, double[] speed, bool possibility_of_movement){
+        if(position[0]==double.NaN | position[1]==double.NaN){
             throw new Exception();
         }
-        else if (speed[0]==NaN & speed[1]=double.Nan)
-        {
+        else if (speed[0]==double.NaN | speed[1]==double.NaN){
             throw new Exception();
         }
-        else if (possibility_of_movement==false)
-        {
+        else if (possibility_of_movement==false){
             throw new Exception();
         }
-        else
-        {
+        else{
             position[0] += speed[0];
             position[1] += speed[1];
             return position;
--- spacebattletests/SpaceBattleTests.cs
+++ spacebattletests/SpaceBattleTests.cs
@@ -6,81 +6,79 @@ using TechTalk.SpecFlow;
 [Binding]
 public class StepDefinitions
 {
-    private double eps = 1e-5;
-    private double[] coef = new double[3];
-    private double[] ans = new double[0];
+    private double[] coords = new double[2];
+    private double[] speed = new double[2];
+    private bool possibility_of_movement = true;
     private Exception r_exp = new Exception();
-    private SquareEquation squareEquation = new SquareEquation();
-    [When("вычисляются корни квадратного уравнения")]
-    public void CalculatedTheRootsOfTheSquareEquation()
+    private SpaceBattle spaceBattle = new SpaceBattle();
+    [When("происходит прямолинейное равномерное движение без деформации")]
+    public void CalculatedTheMovementOfTheSpaceShip()
     {
         try 
         {
-            ans = squareEquation.Solve(coef[0],coef[1],coef[2]);
+            coords = spaceBattle.Movement(coords,speed,possibility_of_movement);
         }
         catch (Exception e)
         {
             r_exp =  e;
         }
     }
-    [Given(@"Квадратное уравнение с коэффициентами \((.*), (.*), (.*)\)")]
-    public void GivenSqaureEquationCoefficients(string a1, string b1, string c1) 
+    [Given(@"космический корабль находится в точке пространства с координатами \((.*), (.*)\)")]
+    public void GivenThePositionOfTheSpaceship(string x, string y) 
     {
-        string[] in_coef = new string[] {a1, b1, c1};
-        
-        for (int i = 0; i < 3; i++)
+        string[] i_coords = new string[2]{x,y}; 
+        for (var i = 0; i<2; i++)
         {
-            if (in_coef[i] == "Double.NegativeInfinity")
-                coef[i] = double.NegativeInfinity;
-            else if (in_coef[i] == "Double.PositiveInfinity")
-                coef[i] = double.PositiveInfinity;
-            else if (in_coef[i] == "NaN")
-                coef[i] = double.NaN;
-            else
-                coef[i] = Convert.ToDouble(in_coef[i]);
+            coords[i] = Convert.ToDouble(i_coords[i]);
         }
     }
-    [Then("выбрасывается исключение ArgumentException")]
-    public void ThrowingArgumentException()
+    [Given(@"имеет мгновенную скорость \((.*), (.*)\)")]
+    public void GivenTheInstantSpeed(string x_speed, string y_speed) 
     {
-        Assert.ThrowsAsync<ArgumentException>(() => throw r_exp);
-    }
-    
-    [Then(@"квадратное уравнение имеет один корень (.*) кратности два")]
-    public void QuadraticEquationHasOneRoot(double x1)
-    {
-        double[] r_ans = new double[] {x1};
-        bool result = false;
-        
-        if (Math.Abs(ans[0] - r_ans[0]) < eps)
+        string[] i_speed = new string[2]{x_speed,y_speed}; 
+        for (var i = 0; i<2; i++)
         {
-            result = true;
+            speed[i] = Convert.ToDouble(i_speed[i]);
         }
-
-        Assert.True(result, "Incorrect");
     }
 
-    [Then(@"квадратное уравнение имеет два корня \((.*), (.*)\) кратности один")]
-    public void QuadraticEquationHasTwoRoots(double x1, double x2)
+    [Given("изменить положение в пространстве космического корабля невозможно")]
+    public void GivenThePositionInSpace() 
+    {
+        possibility_of_movement = false;
+    }
+
+    [Given("космический корабль, положение в пространстве которого невозможно определить")]
+    public void GivenThePositionCannotBeDetermined() 
+    {
+        for(var i=0; i<2; i++)
+        {
+            coords[i] = double.NaN;
+        }
+    }
+
+    [Given("скорость корабля определить невозможно")]
+    public void GivenTheSpeedOfTheShipCannotBeDetermined() 
+    {
+        for(var i=0; i<2; i++)
+        {
+            coords[i] = double.NaN;
+        }
+    }
+
+    [Then(@"возникает ошибка Exception")]
+    public void ThrowingException()
+    {
+        Assert.ThrowsAsync<Exception>(() => throw r_exp);
+    }
+
+    [Then(@"космический корабль перемещается в точку пространства с координатами \((.*), (.*)\)")]
+    public void MovingToAPoint(double x1, double x2)
     {
         double[] r_ans = new double[] {x1, x2};
         bool result = false;
-        Array.Sort(r_ans);
         
-        if (Math.Abs(ans[0] - r_ans[0]) < eps & Math.Abs(ans[1] - r_ans[1]) < eps)
-        {
-            result = true;
-        }
-
-        Assert.True(result, "Incorrect");
-    }
-
-    [Then(@"множество корней квадратного уравнения пустое")]
-    public void SqaureEquationHasNoRoots()
-    {
-        double[] r_ans = new double[]{};
-        bool result = false;
-        if (ans.Length == 0 & r_ans.Length == 0)
+        if (r_ans[0]==coords[0] & r_ans[1]==coords[1])
         {
             result = true;
         }
