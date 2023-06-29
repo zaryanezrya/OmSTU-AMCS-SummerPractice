e14df310f8d17b7a680a20a9663f8366ec919f22
Nikolay Koren <nikolai.koren2004@gmail.com>
2023-06-29 08:57:22 +0000

    Steps 1-3.5

---
+++ SpaceBattle.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace SpaceBattle.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ SpaceBattle/SpaceBattle.cs
@@ -0,0 +1,8 @@
+﻿namespace spacebattle;
+
+public class Spacebattle
+{
+    public static void Movement(int x, int y){
+        
+    }
+}
