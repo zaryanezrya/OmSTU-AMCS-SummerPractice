e4119677361bd4185d2b54def003faa05c6b8373
Dmitriy <n_eco@mail.ru>
2023-06-29 13:02:17 +0000

    тесты написаны но есть ошибки

--- spacebattle/spacebattle.cs
+++ spacebattle/spacebattle.cs
@@ -1,6 +1,17 @@
 ﻿namespace spacebattle;
 
-public class Class1
+public class spacebattle
 {
+    private bool isCanMove, isKnowSpeed, isKnowPosition;
+    double [] Speed=new double [2];
+    double [] Position=new double [2];
 
+    public static double [] Move (bool isCanMove, bool isKnowSpeed,
+    bool isKnowPosition, double [] Speed, double [] Position){
+        if (!isCanMove || !isKnowSpeed || !isKnowPosition){
+            throw new Exception();
+        }
+        double[] total = {Position[0] + Speed[0], Position[1] + Speed[1]};
+        return total;
+    }
 }
