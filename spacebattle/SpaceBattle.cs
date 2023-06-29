1697bb51d01c404a6346d589bc9c315535c0452e
Chixablox <smrdak152@gmail.com>
2023-06-29 09:36:57 +0000

    Класс корабль

--- spacebattle/SpaceBattle.cs
+++ spacebattle/SpaceBattle.cs
@@ -1,5 +1,38 @@
-﻿namespace spacebattle;
-public class Class1
+﻿namespace SpaceBattle;
+public class SpaceBattle
 {
+    private double[] position = new double[2]{double.NaN, double.NaN};
+    private double[] speed = new double[2]{double.NaN, double.NaN};
+    private bool possibility_of_movement = true;
 
-}
+    public SpaceBattle(double x, double y, double x_speed, double y_speed, bool possibility_of_movement)
+    {
+        double n_position = new double[2]{x,y};
+        double n_speed = new double[2]{x_speed,y_speed};
+        this.position = n_position;
+        this.speed = n_speed;
+        this.possibility_of_movement = possibility_of_movement;
+    }
+
+    public double[] Movement (double[] position, double[] speed, bool possibility_of_movement)
+    {
+        if(position[0]==NaN & position[1]=double.Nan)
+        {
+            throw new Exception();
+        }
+        else if (speed[0]==NaN & speed[1]=double.Nan)
+        {
+            throw new Exception();
+        }
+        else if (possibility_of_movement==false)
+        {
+            throw new Exception();
+        }
+        else
+        {
+            position[0] += speed[0];
+            position[1] += speed[1];
+            return position;
+        }
+    }
+ }
--- spacebattletests/SpaceBattleTests.cs
+++ spacebattletests/SpaceBattleTests.cs
@@ -1,10 +1,90 @@
 namespace spacebattletests;
+using SpaceBattle;
+using TechTalk.SpecFlow;
 
-public class UnitTest1
+
+[Binding]
+public class StepDefinitions
 {
-    [Fact]
-    public void Test1()
+    private double eps = 1e-5;
+    private double[] coef = new double[3];
+    private double[] ans = new double[0];
+    private Exception r_exp = new Exception();
+    private SquareEquation squareEquation = new SquareEquation();
+    [When("вычисляются корни квадратного уравнения")]
+    public void CalculatedTheRootsOfTheSquareEquation()
     {
+        try 
+        {
+            ans = squareEquation.Solve(coef[0],coef[1],coef[2]);
+        }
+        catch (Exception e)
+        {
+            r_exp =  e;
+        }
+    }
+    [Given(@"Квадратное уравнение с коэффициентами \((.*), (.*), (.*)\)")]
+    public void GivenSqaureEquationCoefficients(string a1, string b1, string c1) 
+    {
+        string[] in_coef = new string[] {a1, b1, c1};
+        
+        for (int i = 0; i < 3; i++)
+        {
+            if (in_coef[i] == "Double.NegativeInfinity")
+                coef[i] = double.NegativeInfinity;
+            else if (in_coef[i] == "Double.PositiveInfinity")
+                coef[i] = double.PositiveInfinity;
+            else if (in_coef[i] == "NaN")
+                coef[i] = double.NaN;
+            else
+                coef[i] = Convert.ToDouble(in_coef[i]);
+        }
+    }
+    [Then("выбрасывается исключение ArgumentException")]
+    public void ThrowingArgumentException()
+    {
+        Assert.ThrowsAsync<ArgumentException>(() => throw r_exp);
+    }
+    
+    [Then(@"квадратное уравнение имеет один корень (.*) кратности два")]
+    public void QuadraticEquationHasOneRoot(double x1)
+    {
+        double[] r_ans = new double[] {x1};
+        bool result = false;
+        
+        if (Math.Abs(ans[0] - r_ans[0]) < eps)
+        {
+            result = true;
+        }
 
+        Assert.True(result, "Incorrect");
+    }
+
+    [Then(@"квадратное уравнение имеет два корня \((.*), (.*)\) кратности один")]
+    public void QuadraticEquationHasTwoRoots(double x1, double x2)
+    {
+        double[] r_ans = new double[] {x1, x2};
+        bool result = false;
+        Array.Sort(r_ans);
+        
+        if (Math.Abs(ans[0] - r_ans[0]) < eps & Math.Abs(ans[1] - r_ans[1]) < eps)
+        {
+            result = true;
+        }
+
+        Assert.True(result, "Incorrect");
+    }
+
+    [Then(@"множество корней квадратного уравнения пустое")]
+    public void SqaureEquationHasNoRoots()
+    {
+        double[] r_ans = new double[]{};
+        bool result = false;
+        if (ans.Length == 0 & r_ans.Length == 0)
+        {
+            result = true;
+        }
+
+        Assert.True(result, "Incorrect");
     }
 }
