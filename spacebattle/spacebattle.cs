580a7bb29017764f95f18929489347f580ca103a
RiverSpider <raevskih8888@mail.ru>
2023-06-29 14:45:20 +0600

    Solution Task 7

---
+++ spacebattle/spacebattle.cs
@@ -0,0 +1,24 @@
+﻿namespace SpaceBattle;
+public class ShipMove
+{
+    private bool _AllowToMove;
+    private bool _IsSpeed;
+    private bool _IsPosition;
+
+    public double[] _Speed = new double[2];
+    public double[] _Position = new double[2];
+
+    public static double[] ShipMoving(double[] position, double[] speed, bool IsAllow, bool IsPosition, bool IsSpeed){
+        var SM = new ShipMove();
+        SM._AllowToMove = IsAllow;
+        SM._IsPosition = IsPosition;
+        SM._IsSpeed = IsSpeed;
+        SM._Position = position;
+        SM._Speed = speed; // мне кажется, в дальнейшем(если бы эта лаба продолжалась) эти значения можно было использовать в других методах 
+        if (!(SM._AllowToMove) || !(SM._IsPosition) || !(SM._IsSpeed)) throw new Exception();
+        double x = SM._Position[0] + SM._Speed[0];
+        double y = SM._Position[1] + SM._Speed[1];
+        double[] finish = {x, y};
+        return finish; 
+    }
+}
---
+++ spacebattletests/spacebattletests.cs
@@ -0,0 +1,72 @@
+using SpaceBattle;
+using TechTalk.SpecFlow;
+
+namespace SpaceBattleTests{
+[Binding]
+public class ИгровойОбъектМожетПеремещатьсяПоПрямой
+{
+    private readonly ScenarioContext _scenarioContext;
+    private double[] _start = new double[2];
+    private double[] _finish = new double[2];
+    private bool _isAllowToMove = true;
+    private bool _IsSpeed = true;
+    private bool _IsPosition = true; 
+    private double[] _result = new double[2];
+    public ИгровойОбъектМожетПеремещатьсяПоПрямой(ScenarioContext scenarioContext)
+    {
+        _scenarioContext = scenarioContext;
+    }
+    [Given(@"космический корабль находится в точке пространства с координатами \((.*), (.*)\)")]
+    public void ДопустимКосмическийКорабльНаходитсяВТочкеПространстваСКоординатами(double p0, double p1)
+    {
+        _start[0] = p0;
+        _start[1] = p1;
+    }
+
+    [Given(@"имеет мгновенную скорость \((.*), (.*)\)")]
+    public void ИИмеетМгновеннуюСкорость(double p0, double p1)
+    {
+        _finish[0] = p0;
+        _finish[1] = p1;
+    }
+
+    [Given(@"космический корабль, положение в пространстве которого невозможно определить")]
+    public void КосмическийКорабльПоложениеВПространствеКоторогоНевозможноОпределить()
+    {
+        _IsPosition = false;
+    }
+    
+    [Given(@"скорость корабля определить невозможно")]
+    public void CкоростьКорабляОпределитьНевозможно()
+    {
+        _IsSpeed = false;
+    }
+
+    [Given(@"изменить положение в пространстве космического корабля невозможно")]
+    public void ИзменитьПоложениеВПространствеКосмическогоКорабляНевозможно()
+    {
+        _isAllowToMove = false;
+    }
+
+    [When(@"происходит прямолинейное равномерное движение без деформации")]
+    public void ПроисходитПрямолинейноеРавномерноеДвижениеБезДеформации()
+    {
+        try{
+        _result = ShipMove.ShipMoving(_start, _finish, _isAllowToMove, _IsPosition, _IsSpeed);
+        }
+        catch{}
+    }
+
+    [Then(@"космический корабль перемещается в точку пространства с координатами \((.*), (.*)\)")]
+    public void КосмическийКорабльПеремещаетсяВТочкуПространстваСКоординатами(double p0, double p1)
+    {
+        double[] expected = {p0, p1};
+        Assert.Equal(expected, _result);
+    }
+
+    [Then(@"возникает ошибка Exception")]
+    public void ВозникаетОшибкаException ()
+    {
+        Assert.Throws<Exception>(() => ShipMove.ShipMoving(_start, _finish, _isAllowToMove, _IsPosition, _IsSpeed));
+    }
+}}
