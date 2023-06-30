0657ee347673db98bf0f530eb66789ed66cfc7e4
RALINA <seitovaralina1@gmail.com>
2023-06-30 00:42:16 +0000

    Решение задачи Космический бой

---
+++ spacebattle/spacebattle.cs
@@ -0,0 +1,24 @@
+﻿namespace spacebattle;
+public class SpaceBattle
+{
+    double[] point = {double.NaN, double.NaN};
+    public double[] Point {set {point = value;}}
+    double[] speed = {double.NaN, double.NaN};
+    public double[] Speed {set {speed = value;}}
+    bool change_position = true;
+    public bool Change_position {set {change_position = value;}}
+    bool HasNormalValue(double[] a)
+    {
+        return !double.IsNaN(a[0]) && !double.IsNaN(a[1]);
+    }
+    public double[] Uniform_motion()
+    {
+        if (HasNormalValue(point) && HasNormalValue(speed) && change_position)
+        {
+            point[0] = point[0] + speed[0];
+            point[1] = point[1] + speed[1];
+        }
+        else { throw new Exception(); }
+        return point;
+    }
+}
