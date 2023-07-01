7ec67ba8d468544ab7dc1416db67db549f738e23
Кирилл <pote1357@mail.ru>
2023-07-01 18:59:39 +0300

    com

---
+++ solution/SpaceBattle/SpaceBattle.cs
@@ -0,0 +1,12 @@
+﻿namespace SpaceBattle {
+public class Spacebattle {
+    public static double[] ship(double[] coordinates, double[] speed, bool Position, bool Speed, bool Movement) {
+        if (!Position || !Speed || !Movement) {
+            throw new System.Exception();
+        }
+        coordinates[0] += speed[0];
+        coordinates[1] += speed[1];
+        return coordinates;
+    }
+}
+}
