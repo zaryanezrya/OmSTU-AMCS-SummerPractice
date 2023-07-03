13f4e55b883f9576aad8d8ff59a1045956896d36
n1yatsh1n <niyatshinemil43@gmail.com>
2023-07-03 21:47:41 +0000

    tests

---
+++ spaceBattle/Class1.cs
@@ -0,0 +1,34 @@
+﻿﻿namespace spaceBattle
+{
+    public class SpaceShip
+    {
+        private double[]? startCoordinates = null;
+        private double[]? speedCoordinates = null;
+        private bool abilityToMove = true;
+
+        public void SetCoordinates(double x, double y)
+        {
+            startCoordinates = new double[] { x, y };
+        }
+
+        public void SetSpeedCoordinates(double x, double y)
+        {
+            speedCoordinates = new double[] { x, y };
+        }
+
+        public void NotMoving()
+        {
+            abilityToMove = false;
+        }
+
+        public double[] MovingAction()
+        {
+            if (startCoordinates is not null & speedCoordinates is not null & abilityToMove)
+            {
+                return new double[] { startCoordinates[0] + speedCoordinates[0], startCoordinates[1] +speedCoordinates[1] };
+            }
+
+            else { throw new Exception("check the values, maybe something is set as null!"); }
+        }
+    }
+}
