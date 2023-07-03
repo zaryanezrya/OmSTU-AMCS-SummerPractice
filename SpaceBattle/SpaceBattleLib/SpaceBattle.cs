8814d4db79ecc0a6be408c407c54931802cd654f
Maksat <zenfonemaxprom124@gmail.com>
2023-07-03 15:55:28 +0000

    v1

---
+++ SpaceBattle/SpaceBattleLib/SpaceBattle.cs
@@ -0,0 +1,21 @@
+﻿namespace SpaceBattleLib;
+
+public class SpaceBattle
+{
+    public static double[] Movement(double[] Coordinates, double[] Speed, bool relocate)
+    {
+        foreach(double x in Coordinates.Concat(Speed).ToArray())
+        {
+            if (double.IsNaN(x) | double.IsInfinity(x)) throw new System.Exception();
+        }
+
+        if (relocate == false) throw new System.Exception();
+
+        for (int i = 0; i < Coordinates.Length;i++)
+        {
+            Coordinates[i] += Speed[i];
+        }
+
+        return  Coordinates;
+    }
+}
