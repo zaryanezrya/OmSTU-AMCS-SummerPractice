2752c0482f1fd7a76df60b0ebdf5a62e9655b7f2
QFormX <mr.bogdan2003@gmail.com>
2023-07-05 01:39:45 +0600

    Pool and Pool guard update

---
+++ spacebattle/Pool.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using spacebattle;
+
+namespace spacebattle
+{
+    internal class Pool<T> where T : class, new()
+    {
+       private readonly Stack<T> _pool;
+
+        public Pool(int count)
+        {
+            _pool = new Stack<T>(count);
+        }
+
+        public T GetObject()
+        {
+            if (_pool.Count > 0)
+            {
+                return _pool.Pop();
+            }
+            return new T();
+        }
+
+        public void ReleaseObject(T obj)
+        {
+            _pool.Push(obj);
+        }
+    }
+}
---
+++ spacebattle/PoolGuard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spacebattle
+{
+    internal class PoolGuard<T> : IDisposable where T : class, new()
+    {
+        private readonly Pool<T> _pool;
+        private readonly T _obj;
+
+        public PoolGuard(Pool<T> pool)
+        {
+            _pool = pool;
+            _obj = _pool.GetObject();
+        }
+
+        public T Object => _obj;
+
+        public void Dispose()
+        {
+            _pool.ReleaseObject(_obj);
+        }
+    }
+}
