851991e9d6c1255ef0f10d21c1cb52b85cc0fbb3
Dmitry <dimkad1810@mail.ru>
2023-07-05 11:53:10 +0000

    space_8

--- SpaceBattle.Tests/StepDifinitions.cs
+++ SpaceBattle.Tests/StepDifinitions.cs
@@ -2,7 +2,7 @@ namespace SpaceBattle.Tests;
 using TechTalk.SpecFlow;
 using SpaceBattleLib;
 
-[Binding]
+[Binding, Scope(Feature = "Движение корабля")]
 public class StepDefinitions
 {
     private double[] spaceshipCoordinates = new double[]{double.NaN, double.NaN};
@@ -40,7 +40,7 @@ public class StepDefinitions
     [When(@"^происходит прямолинейное равномерное движение без деформации")]
     public void ДвижениеКорабля(){
         try{
-            actualResult = SpaceBattle.Movement(spaceshipCoordinates, spaceshipSpeed, relocate);
+            actualResult = ShipMove.Movement(spaceshipCoordinates, spaceshipSpeed, relocate);
         }
         catch(Exception e){
             actualException = e;
@@ -55,6 +55,105 @@ public class StepDefinitions
         for (int i = 0; i < expectedResult.Length;i++){
                 Assert.Equal(expectedResult[i], actualResult[i], 6);
             }
+    }
+
+    [Then(@"возникает ошибка Exception")]
+    public void ВозникаетОшибка()
+    {
+        Assert.ThrowsAsync<Exception>(() => throw actualException);
+    }
+}
+
+[Binding, Scope(Feature = "Движение при достаточном количестве топлива")]
+public class StepDefinitions2
+{
+    private double fuelAmount = double.NaN;
+    private double fuelConsumption = double.NaN;
+    private Exception actualException = new Exception();
+    private double actualResult = double.NaN; 
+
+	[Given(@"^космический корабль имеет топливо в объеме (.*) ед")]
+    public void КоординатыКорабляВТочке(double fuelAm)
+    { 
+        fuelAmount = fuelAm; 
+    }
+
+    [Given(@"^имеет скорость расхода топлива при движении (.*) ед")]
+    public void СкоростьКорабля(double fuelCons)
+    { 
+        fuelConsumption = fuelCons;  
+    }
+
+    [When(@"^происходит прямолинейное равномерное движение без деформации")]
+    public void ДвижениеКорабля(){
+        try{
+            actualResult = ShipFuel.Movement(fuelAmount, fuelConsumption);
+        }
+        catch(Exception e){
+            actualException = e;
+        }
+    }
+
+    [Then(@"новый объем топлива космического корабля равен (.*) ед")]
+    public void КосмическийКорабльПеремещаетсяВТочкуПространства(double expectedResult)
+    {
+        Assert.Equal(expectedResult, actualResult, 6);
+    }
+
+    [Then(@"возникает ошибка Exception")]
+    public void ВозникаетОшибка()
+    {
+        Assert.ThrowsAsync<Exception>(() => throw actualException);
+    }
+}
+
+[Binding, Scope(Feature = "Разворот")]
+public class StepDefinitions3
+{
+    private double inclination = double.NaN;
+    private double rotationalSpeed = double.NaN;
+    private double actualResult = double.NaN;
+    private bool rotation = true;
+    private Exception actualException = new Exception();
+
+	[Given(@"^космический корабль имеет угол наклона (.*) град к оси OX")]
+    public void УголНаклона(double incl)
+    { 
+        inclination = incl;
+    }
+
+    [Given(@"^космический корабль, угол наклона которого невозможно определить")]
+    public void УголНаклонаОпределитьНевозможно(){}
+
+    [Given(@"^имеет мгновенную угловую скорость (.*) град")]
+    public void УгловаяСкорость(double rotSpeed)
+    { 
+        rotationalSpeed = rotSpeed;
+    }
+
+    [Given(@"^мгновенную угловую скорость невозможно определить")]
+    public void СкоростьВращения(){}
+
+    [Given(@"невозможно изменить уголд наклона к оси OX космического корабля")]
+         public void НевозможноИзменитьУголдНаклонаКОсиOXКосмическогоКорабля()
+         {
+            rotation = false;
+         }
+
+    [When(@"^происходит вращение вокруг собственной оси")]
+    public void Вращение(){
+        try{
+            actualResult = ShipRotation.Rotation(inclination, rotationalSpeed, rotation);
+        }
+        catch(Exception e){
+            actualException = e;
+        }
+    }
+
+    [Then(@"угол наклона космического корабля к оси OX составляет (.*) град")]
+    public void УголНаклонаСтановится(double expectedResult)
+    {
+        Assert.Equal(expectedResult, actualResult, 6);
     }
 
     [Then(@"возникает ошибка Exception")]
--- SpaceBattle/SpaceBattle.cs
+++ SpaceBattle/SpaceBattle.cs
@@ -1,18 +1,41 @@
 namespace SpaceBattleLib;
 
-public class SpaceBattle
+public class ShipMove
 {
-    public static double[] Movement(double[] SpaceshipCoordinates, double[] SpaceshipSpeed, bool relocate)
+    public static double[] Movement(double[] spaceshipCoordinates, double[] spaceshipSpeed, bool relocate)
     {
-        foreach(double x in SpaceshipCoordinates.Concat(SpaceshipSpeed).ToArray()){
+        foreach(double x in spaceshipCoordinates.Concat(spaceshipSpeed).ToArray()){
             if (double.IsNaN(x) | double.IsInfinity(x)) throw new System.Exception();
         }
         if (relocate == false) throw new System.Exception();
 
-        for (int i = 0; i < SpaceshipCoordinates.Length;i++){
-            SpaceshipCoordinates[i] += SpaceshipSpeed[i];
+        for (int i = 0; i < spaceshipCoordinates.Length;i++){
+            spaceshipCoordinates[i] += spaceshipSpeed[i];
         }
 
-        return  SpaceshipCoordinates;
+        return  spaceshipCoordinates;
+    }
+}
+
+
+public class ShipFuel
+{
+    public static double Movement(double fuelAmount, double fuelConsumption){
+        if (fuelConsumption > fuelAmount) throw new System.Exception();
+
+        return fuelAmount - fuelConsumption;
+    }
+}
+
+
+public class ShipRotation
+{
+    public static double Rotation(double inclination, double rotationalSpeed, bool rotation){
+        foreach(double x in new double[]{inclination, rotationalSpeed}){
+            if (double.IsNaN(x) | double.IsInfinity(x)) throw new System.Exception();
+        }
+        if (rotation == false) throw new System.Exception();
+
+        return inclination + rotationalSpeed;
     }
 }
