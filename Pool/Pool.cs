4817c1d1b21a63b685792f4abdfb674cd45d0862
Maksat <zenfonemaxprom124@gmail.com>
2023-07-05 14:01:14 +0000

    v1

---
+++ Pool/Pool.cs
@@ -0,0 +1,75 @@
+﻿namespace Pool;
+using SpaceBattleLib;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int poolSize;
+        try 
+        {
+            poolSize = int.Parse(args[0]);
+        }
+        catch
+        {
+            poolSize = 10;
+        }
+        
+        Pool<Spaceship> spaceshipPool = new Pool<Spaceship>(poolSize);
+        using (PoolGuard<Spaceship> guard = new PoolGuard<Spaceship>(spaceshipPool))
+        {
+            Spaceship spaceship = guard.Object;
+        }
+    }
+}
+
+public class Pool<T> where T : new()
+{
+    private readonly Queue<T> pool;
+
+    public Pool(int size)
+    {
+        pool = new Queue<T>(size);
+        for (int i = 0; i < size; i++)
+        {
+            pool.Enqueue(new T());
+        }
+    }
+
+    public T GetObject()
+    {
+        if (pool.Count > 0)
+        {
+            return pool.Dequeue();
+        }
+        else
+        {
+            return new T();
+        }
+    }
+
+    public void ReturnObject(T obj)
+    {
+        pool.Enqueue(obj);
+    }
+}
+
+public class PoolGuard<T> : IDisposable where T : new()
+{
+    private readonly Pool<T> pool;
+    private readonly T obj;
+
+    public PoolGuard(Pool<T> pool)
+    {
+        this.pool = pool;
+        obj = pool.GetObject();
+    }
+
+    public T Object => obj;
+
+    public void Dispose()
+    {
+        pool.ReturnObject(obj);
+    }
+}
